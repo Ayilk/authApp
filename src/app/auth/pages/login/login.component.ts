205ab4a1e48b5054dec12ea3914e8c93dcb3dcdd
Ayilk <srangel.h92@gmail.com>
2022-08-04 00:00:10 -0500

    Login de usuario desde angular

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,13 +19,20 @@ export class LoginComponent  {
 
 
   constructor(private fb: FormBuilder,
-              private router: Router) { }
+              private router: Router,
+              private auhtService: AuthService) { }
 
   login(){
     console.log(this.miFormulario.value);
     console.log(this.miFormulario.valid);
+    
+    const {email, password} = this.miFormulario.value;
+    this.auhtService.login(email,password)
+    .subscribe(resp => {
+      console.log(resp)
+    })
 
-    this.router.navigateByUrl('/dashboard');
+    // this.router.navigateByUrl('/dashboard');
   }
 
   
