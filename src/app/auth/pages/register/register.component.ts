bcacec0b05dbf29bef9ecde7fa32cacd8f759327
Ayilk <srangel.h92@gmail.com>
2022-08-05 16:56:30 -0500

    Registro de usuarios

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,20 +17,34 @@ export class RegisterComponent  {
   
 
   miFormulario: FormGroup = this.fb.group({
-    name: ['Fulanita Chavez', [Validators.required]],
-    email: ['test1@test.com', [Validators.required, Validators.email]],
-    password: ['256897', [Validators.required, Validators.minLength(6)]]
+    name: ['Valeria', [Validators.required]],
+    email: ['valeria@mail.com', [Validators.required, Validators.email]],
+    password: ['246810', [Validators.required, Validators.minLength(6)]]
   })
 
 
   constructor(private fb: FormBuilder,
-              private router: Router) { }
+              private router: Router,
+              private  authService: AuthService) { }
 
   registro(){
     console.log(this.miFormulario.value);
     console.log(this.miFormulario.valid);
 
-    this.router.navigateByUrl('/dashboard');
+    const { name, email, password } = this.miFormulario.value;
+
+    this.authService.register(name, email,password )
+    .subscribe(ok => {
+      console.log("ok = ",ok)
+      
+      if(ok === true){
+        this.router.navigateByUrl('/dashboard')
+      }else{
+        //Mostrar el mensjae de error
+        Swal.fire('Error', ok, 'error')
+      }
+    })
+   // this.router.navigateByUrl('/dashboard');
   }
 
 
