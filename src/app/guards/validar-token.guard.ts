f6b1b3890d93d0f25a338e57f01d1271cf911908
Ayilk <srangel.h92@gmail.com>
2022-08-05 16:17:12 -0500

    Validar token - gard

---
+++ src/app/guards/validar-token.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarTokenGuard implements CanActivate, CanLoad {
+
+  constructor(private authService: AuthService,
+              private router: Router){}
+
+  canActivate(): Observable<boolean > | boolean  {
+    console.log('canActivate');
+
+    //Si esto me regresa false, significa que el usuario no esta autenticado y lo voy a sacar de ahí
+    return this.authService.validarToken()
+    .pipe(
+      tap(valid => {
+        if(!valid){
+          this.router.navigateByUrl('/auth')
+        }
+      })
+    )
+
+  }
+  canLoad(): Observable<boolean > | boolean {
+    console.log('canLoad');
+    return true;
+  }
+}
